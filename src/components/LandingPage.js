8c7fdab5d558bf19468ff2b393b20f6567cab636
Reizetshu <114964887+Reizetshu@users.noreply.github.com>
2023-01-09 12:08:23 +0800

    Update LandingPage.js
    Fix: Corrected the spelling of now.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -21,7 +21,7 @@ const LandingPage = () => {
                     </span>
                     &gt;
                 </h4>
-                <p className='landing-txt'>My name is Mark Benson B. Hernando. I recently graduated as a <span className='landing-span'>Computer Engineering</span>. <br className='none'/>I am a Fullstack Web Developer especially <span className='landing-span'>MERN Stack</span>. I am seeking for knowledge all the <br className='none'/>things that peeks my interest but for know I want to focus on my web development career.</p>
+                <p className='landing-txt'>My name is Mark Benson B. Hernando. I recently graduated as a <span className='landing-span'>Computer Engineering</span>. <br className='none'/>I am a Fullstack Web Developer especially <span className='landing-span'>MERN Stack</span>. I am seeking for knowledge all the <br className='none'/>things that peeks my interest but for now I want to focus on my web development career.</p>
                 <h4 className='about-me'>
                     &lt;/
                     <span className='about-me-inner'>
